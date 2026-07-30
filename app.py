d1fa06a620063d3e0bd15d7e2c1a56816758dfd3
Nour <nourelsayed2005farah@gmail.com>
2026-07-30 18:01:43 +0300

    Fix SECRET_KEY syntax

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 app.secret_key = os.environ.get(
     "SECRET_KEY",
-app.secret_key = os.environ.get("SECRET_KEY")
+    secrets.token_hex(32)
 )
 mail = Mail(app)
 serializer = URLSafeTimedSerializer(app.secret_key)
